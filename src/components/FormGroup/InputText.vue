83c9c07633b33c05c74b460f77539d2bc285ff4b
srvaz <jonwify@gmail.com>
2020-05-17 20:10:33 -0300

    :lipstick: Add input text components

---
+++ src/components/FormGroup/InputText.vue
@@ -0,0 +1,44 @@
+<template>
+  <input :type="type" :placeholder="placeholder" class="input-text" />
+</template>
+
+<script>
+export default {
+  name: 'InputText',
+  props: {
+    type: {
+      type: String,
+      defaultL: 'text',
+      validator: (type) => ['text', 'number'].includes(type),
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../utils/scss/index.scss';
+
+.input-text {
+  height: 35px;
+  border: 1px solid rgba($color: #000000, $alpha: 0.16);
+  font-size: var(--font-size-default);
+  padding: var(--space-sm);
+  @include border-radius(light);
+
+  &:hover {
+    border-color: var(--color-text-dark);
+  }
+
+  &:focus {
+    border-color: var(--color-primary);
+  }
+
+  &::placeholder {
+    color: var(--color-text-gray);
+  }
+}
+</style>
---
+++ src/components/FormGroup/Textarea.vue
@@ -0,0 +1,41 @@
+<template>
+  <textarea :placeholder="placeholder" class="textarea" />
+</template>
+
+<script>
+export default {
+  name: 'Textarea',
+  props: {
+    placeholder: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../../utils/scss/index.scss';
+
+.textarea {
+  height: 35px;
+  border: 1px solid rgba($color: #000000, $alpha: 0.16);
+  font-size: var(--font-size-default);
+  padding: var(--space-sm);
+  height: 114px;
+  max-height: 114px;
+  @include border-radius(light);
+
+  &:hover {
+    border-color: var(--color-text-dark);
+  }
+
+  &:focus {
+    border-color: var(--color-primary);
+  }
+
+  &::placeholder {
+    color: var(--color-text-gray);
+  }
+}
+</style>
