3764fa864708a94941523156a42127e533365582
srvaz <jonwify@gmail.com>
2020-05-17 20:12:49 -0300

    :lipstick: Add SelectButton component

---
+++ src/components/FormGroup/SelectButton.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="select-button-container">
+    <div
+      v-for="(option, index) in options"
+      :key="index"
+      class="select-button-wrapper"
+    >
+      <input
+        :id="`opt-${index}`"
+        name="opt"
+        type="radio"
+        :value="option.value"
+        class="select-button"
+      />
+      <label class="select-button__label" :for="`opt-${index}`">{{
+        option.label
+      }}</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SelectButton',
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../utils/scss/index.scss';
+
+.select-button-container {
+  display: grid;
+  gap: var(--space-sm);
+  grid-auto-flow: column;
+  justify-content: start;
+
+  .select-button-wrapper {
+    position: relative;
+
+    .select-button {
+      appearance: none;
+      position: absolute;
+
+      &:checked + .select-button__label {
+        background-color: var(--color-primary);
+      }
+    }
+
+    .select-button__label {
+      cursor: pointer;
+      width: 35px;
+      height: 35px;
+      font-weight: var(--font-weight-medium);
+      background-color: var(--color-white);
+      border: 1px solid rgba($color: #000000, $alpha: 0.16);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      @include border-radius(light);
+
+      &:hover {
+        background-color: var(--color-text-disabled);
+      }
+    }
+  }
+}
+</style>
